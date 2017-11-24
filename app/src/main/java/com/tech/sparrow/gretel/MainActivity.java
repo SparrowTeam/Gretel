ca82a2e76e69e611f2b82b8f5b79e42a204e45f3
takoz <tuonomule@gmail.com>
2017-11-24 23:03:12 +0300

    Initial commit

---
+++ app/src/main/java/com/tech/sparrow/gretel/MainActivity.java
@@ -0,0 +1,48 @@
+package com.tech.sparrow.gretel;
+
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+    public LoyaltyCardReader mLoyaltyCardReader;
+    public static int READER_FLAGS =
+            NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        NfcAdapter nfc = NfcAdapter.getDefaultAdapter(this);
+        mLoyaltyCardReader = new LoyaltyCardReader();
+        if (nfc != null) {
+            nfc.enableReaderMode(this, mLoyaltyCardReader, READER_FLAGS, null);
+        }
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (intent != null && NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+            resolveIntent(intent);
+        }
+    }
+
+    private void resolveIntent(Intent intent) {
+        Tag tag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        if (tag != null) {
+            byte[] tagId = tag.getId();
+            Log.i(TAG, String.format("ID: %02X %02X %02X %02X",
+                    tagId[0],
+                    tagId[1],
+                    tagId[2],
+                    tagId[3]
+                    )
+            );
+        }
+    }
+}
