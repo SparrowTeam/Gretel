da25882c8e0b00407821c9b1f3e45fb36c3e35cb
takoz <tuonomule@gmail.com>
2017-11-25 00:20:26 +0300

    Добавлена Activity с картой

--- app/src/main/java/com/tech/sparrow/gretel/MainActivity.java
+++ app/src/main/java/com/tech/sparrow/gretel/MainActivity.java
@@ -6,6 +6,9 @@ import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
@@ -33,16 +36,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void resolveIntent(Intent intent) {
-        Tag tag = (Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
         if (tag != null) {
             byte[] tagId = tag.getId();
-            Log.i(TAG, String.format("ID: %02X %02X %02X %02X",
-                    tagId[0],
-                    tagId[1],
-                    tagId[2],
-                    tagId[3]
-                    )
+            String tagIdStr = String.format("%02X %02X %02X %02X",
+                tagId[0],
+                tagId[1],
+                tagId[2],
+                tagId[3]
             );
+            Log.i(TAG, "ID: " + tagIdStr);
+            setContentView(R.layout.activity_main);
+            TextView markTextView = findViewById(R.id.markIDtextView);
+            markTextView.setText(tagIdStr);
         }
+    }
+
+    public void handleClick(View view) {
+        Intent i = new Intent(getBaseContext(), MapsActivity.class);
+        startActivity(i);
     }
 }
