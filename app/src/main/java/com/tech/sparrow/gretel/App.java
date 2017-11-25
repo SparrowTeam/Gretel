52b815d91fe550afecba6dee72161d74f34a4903
takoz <tuonomule@gmail.com>
2017-11-25 21:58:46 +0300

    Загрузка фото пользователя

--- app/src/main/java/com/tech/sparrow/gretel/App.java
+++ app/src/main/java/com/tech/sparrow/gretel/App.java
@@ -13,7 +13,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class App extends Application {
 
-    private static String endpoint_url = "http://10.100.50.148:8080/";
+    private static String endpoint_url = "http://10.100.16.26:8080/";
     private static HanselService hanselService;
     private static Retrofit retrofit;
 
--- app/src/main/java/com/tech/sparrow/gretel/UserActivity.java
+++ app/src/main/java/com/tech/sparrow/gretel/UserActivity.java
@@ -1,13 +1,83 @@
 package com.tech.sparrow.gretel;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.util.SortedList;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserActivity extends AppCompatActivity {
+    public static final String TAG = "UserActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+    }
+
+    public void onImageClick(View view) {
+        startActivityForResult(
+                new Intent(
+                        Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI
+                ),
+                3
+        );
+    }
+
+    private String getRealPathFromURIPath(Uri contentURI, Activity activity) {
+        Cursor cursor = activity.getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) {
+            return contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(idx);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==3 && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            String filePath = getRealPathFromURIPath(selectedImage, this);
+            File file = new File(filePath);
+
+            RequestBody reqFile = RequestBody.create(MediaType.parse("image/*"), file);
+            MultipartBody.Part body = MultipartBody.Part.createFormData("upload", file.getName(), reqFile);
+            RequestBody name = RequestBody.create(MediaType.parse("text/plain"), "upload_test");
+
+            retrofit2.Call<okhttp3.ResponseBody> req = App.getApi().postImage("Zm9vMkBiYWF6LnJ1", body, name);
+            req.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    Log.d(TAG, "Response: " + response.toString());
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    t.printStackTrace();
+                }
+            });
+        }
     }
 }
